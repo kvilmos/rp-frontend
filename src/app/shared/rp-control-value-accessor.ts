c8bc0a605ef93ab570ed587fe3192d21153cd432
kvilmos <kvilmos47@gmail.com>
2025-09-20 07:15:01 +0200

    feat: Add control value accessor directive and text input.

---
+++ src/app/shared/rp-control-value-accessor.ts
@@ -0,0 +1,78 @@
+import { Directive, Inject, Injector, OnDestroy, OnInit } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormControlDirective,
+  FormControlName,
+  FormGroupDirective,
+  NgControl,
+  Validators,
+} from '@angular/forms';
+import { Subject, takeUntil, startWith, distinctUntilChanged, tap } from 'rxjs';
+
+@Directive({
+  standalone: true,
+  selector: '[rpControlValueAccessor]',
+})
+export class RpControlValueAccessor<T> implements ControlValueAccessor, OnInit, OnDestroy {
+  public control: FormControl | undefined;
+  public isRequired = false;
+
+  private isDisabled = false;
+  private destroy$ = new Subject<void>();
+  public onTouched!: () => T;
+
+  constructor(@Inject(Injector) private injector: Injector) {}
+
+  public ngOnInit() {
+    this.setFormControl();
+    this.isRequired = this.control?.hasValidator(Validators.required) ?? false;
+  }
+
+  private setFormControl() {
+    try {
+      const formControl = this.injector.get(NgControl);
+
+      switch (formControl.constructor) {
+        case FormControlName:
+          this.control = this.injector
+            .get(FormGroupDirective)
+            .getControl(formControl as FormControlName);
+          break;
+        default:
+          this.control = (formControl as FormControlDirective).form as FormControl;
+          break;
+      }
+    } catch (err) {
+      this.control = new FormControl();
+    }
+  }
+
+  public writeValue(value: T): void {
+    this.control ? this.control.setValue(value) : (this.control = new FormControl(value));
+  }
+
+  public registerOnChange(fn: (val: T | null) => T): void {
+    this.control?.valueChanges
+      .pipe(
+        takeUntil(this.destroy$),
+        startWith(this.control.value),
+        distinctUntilChanged(),
+        tap((val) => fn(val))
+      )
+      .subscribe(() => this.control?.markAsUntouched());
+  }
+
+  public registerOnTouched(fn: () => T): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
