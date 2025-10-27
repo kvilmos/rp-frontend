787330cd71217fb8221552b924a9e012a4bdda09
kvilmos <kvilmos47@gmail.com>
2025-10-27 19:49:45 +0100

    feat: Add style to home page.

--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,28 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCouch, faHouseChimneyWindow, faPenRuler } from '@fortawesome/free-solid-svg-icons';
+import { TranslatePipe } from '@ngx-translate/core';
+import { BlueprintApiService } from '../planner/blueprint-api-service';
+import { CompleteBlueprint } from '../planner/blueprint_load';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [],
+  imports: [FontAwesomeModule, TranslatePipe],
 })
 export class Home {
+  public addIcon = faPenRuler;
+  public furnitureIcon = faCouch;
+  public blueprintIcon = faHouseChimneyWindow;
+
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly bpApi = inject(BlueprintApiService);
+
   constructor() {}
 
-  public onClickLogout(): void {
-    this.authService.logout();
-  }
-
-  public onClickFurnitureUpload(): void {
-    this.router.navigate(['furniture-upload']);
-  }
-
-  public onClickPlanner(): void {
-    this.router.navigate(['planner']);
+  public onCreateBlueprint(): void {
+    this.bpApi.createBlueprint().subscribe({
+      next: (blueprint: CompleteBlueprint) => {
+        this.router.navigate(['/room-editor', blueprint.id]);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 }
