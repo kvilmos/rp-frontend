39276cf5f2f234ef31509c9d9fcb72809d394b2b
kvilmos <kvilmos47@gmail.com>
2025-11-09 10:32:39 +0100

    feat: Add snackbar message on logout.

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RpNavbar } from '../../shared/navbar/navbar';
+import { RpNavbar } from '../../shared/rp-navbar/rp-navbar';
 
 @Component({
   standalone: true,
